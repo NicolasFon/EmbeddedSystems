94b4aa3ec8bb0fde5a06ab7d4397dcfa62ba3fad
NicolasFon <n-fontaine@hotmail.fr>
2019-03-25 12:02:43 +0100

    Create Projet.php

---
+++ Projet.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+        <head>
+                <meta charset="utf-8">
+                <title>Contrôle Servo</title>
+                <link rel="Projet" type="text/css" href="Projet.css">
+        </head>
+        <body>
+                <form action="Script.php" method="post">
+                        <input type="submit" name="executer" value="LedOn" class="button" id="LedOn">
+                        <input type="submit" name="executer" value="LedOff" class="button" id="LedOff">
+                        <br/>
+                        <input type="submit" name="executer" value="SwingCradle" class="button" id="SwingCradle">
+                        <br/>
+                        <input type="submit" name="executer" value="Music" class="button" id="Music">
+               </form>
+                        <input type="button" onclick="window.open(':8081');" name="executer" value="Camera" id="Camer$         </body>
+</html>
